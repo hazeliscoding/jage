5194b26c5b336c24e7b6c61871839f36f622ab12
Hazel Granados <hazel.granados@protonmail.com>
2024-06-26 14:50:22 -0500

    feat: defining api

--- jage/include/jage/input/joystick.h
+++ jage/include/jage/input/joystick.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <array>
+#include <unordered_map>
+#include <memory>
+
+struct SDL_ControllerDeviceEvent;
+typedef struct _SDL_GameController SDL_GameController;
+
 namespace jage::input
 {
 	class JoyStick
@@ -39,7 +46,30 @@ namespace jage::input
 		};
 
 	public:
+		static void OnJoystickConnected(SDL_ControllerDeviceEvent& e);
+		static void OnJoystickDisconnected(SDL_ControllerDeviceEvent& e);
+		static void Shutdown();
+		static void Update();
+
+		static bool GetButton(int joystick_id, Button button);
+		static bool GetButtonDown(int joystick_id, Button button);
+		static bool GetButtonUp(int joystick_id, Button button);
+		static float GetAxis(int joystick_id, Axis axis);
+
+	private:
+		static int GetNextFreeIndex();
 		
 	private:
+		struct Controller
+		{
+			SDL_GameController* gc = nullptr;
+
+			std::array<bool, static_cast<int>(Button::Count)> buttons;
+			std::array<bool, static_cast<int>(Button::Count)> last_buttons;
+			std::array<bool, static_cast<int>(Axis::Count)> axes;
+			std::array<bool, static_cast<int>(Axis::Count)> last_axes;
+		};
+
+		static std::unordered_map<int, std::unique_ptr<Controller>> available_joysticks_;
 	};
 }
